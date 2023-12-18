d091543c899e7cad41fcc79761fe1f4c3de9e463
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-12-18 21:48:06 +0530

    Create Check if strings are rotations of each other or not.py

---
+++ Check if strings are rotations of each other or not.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        #code here
+        result = s1+s1 
+        if s2 in result:
+            return 1 
+        return 0
