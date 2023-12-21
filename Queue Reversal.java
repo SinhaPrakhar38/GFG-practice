9d862ce3fc38221079389a7a56732c6f34795901
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-12-21 22:11:12 +0530

    Update Queue Reversal.java

--- Queue Reversal.java
+++ Queue Reversal.java
@@ -2,14 +2,24 @@ class GfG{
     //Function to reverse the queue.
     public Queue<Integer> rev(Queue<Integer> q){
         //add code here.
-        Stack<Integer> stack = new Stack<>();
+         //create a stack for reversing the queue
+      Stack <Integer> s = new Stack<>();
      
-        while(queue.size > 0) {
-           stack.push(queue.poll());
-        }
-        while(stack.size() > 0) {
-           queue.add(stack.pop());
-        }
-        return queue;
+      /*while the queue is not empty , pop(offer) the front element
+      from queue and insert(push) that element into the stack. */
+
+      while(!q.isEmpty())
+      {
+        s.push(q.poll());  //Inserting into stack
+      }
+
+      /* Till the stack is not empty , pop the element from 
+      stack and insert(offer) it into the queue. */
+
+      while(!s.isEmpty())
+      {
+        q.offer(s.pop());  //Inserting into queue
+      }
+      return q;
     }
 }
